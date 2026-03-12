4b3545e1285de6358f24896713b910ae1bda2e1f
Timothée-BN <timothee.nicolet@fdti.eu>
2026-03-12 01:34:33 +0100

    update deploy img

--- src/lib/utils/pocketbase.ts
+++ src/lib/utils/pocketbase.ts
@@ -1,8 +1,9 @@
 import PocketBase from 'pocketbase';
 import { env } from '$env/dynamic/private';
+import { env as publicEnv } from '$env/dynamic/public';
 
 export const pb = new PocketBase(env.PB_URL);
-const pbPublicBaseUrl = (env.PUBLIC_PB_URL || env.PB_URL).replace(/\/$/, '');
+const pbPublicBaseUrl = (publicEnv.PUBLIC_PB_URL || env.PB_PUBLIC_URL || env.PB_URL).replace(/\/$/, '');
 
 export function getPocketBaseFileUrl(record: { collectionId: string; id: string }, filename: string) {
     return `${pbPublicBaseUrl}/api/files/${record.collectionId}/${record.id}/${filename}`;
