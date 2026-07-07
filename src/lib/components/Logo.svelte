340462cd68b75b6f719a585cf2940d7f3f4dbcd8
Timothée-BN <timothee.nicolet@fdti.eu>
2026-07-07 19:30:05 +0200

    Allow custom logo image classes

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -5,16 +5,24 @@
 		alt,
 		size = 'w-30 h-20',
 		href,
-		classOverride
-	}: { src: string; alt: string; size?: string; href?: string; classOverride?: string } = $props();
+		classOverride,
+		imageClass = ''
+	}: {
+		src: string;
+		alt: string;
+		size?: string;
+		href?: string;
+		classOverride?: string;
+		imageClass?: string;
+	} = $props();
 </script>
 
 {#if href}
 	<a {href} class={`${size} ${classOverride}`}>
-		<img {src} {alt} class="h-full w-full object-contain" fetchpriority="high" />
+		<img {src} {alt} class={`h-full w-full object-contain ${imageClass}`} fetchpriority="high" />
 	</a>
 {:else}
 	<div class={`${size} ${classOverride}`}>
-		<img {src} {alt} class="h-full w-full object-contain" fetchpriority="high" />
+		<img {src} {alt} class={`h-full w-full object-contain ${imageClass}`} fetchpriority="high" />
 	</div>
 {/if}
